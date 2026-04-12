144add5f660a8e7fbb68f9caeaf851b8858f703b
Mohit <mohitbaghel1234321@gmail.com>
2026-04-12 15:52:06 +0530

    Complete

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -37,26 +37,32 @@ const Contact = () => {
 
         {/* Social Media Holders */}
         <a href="https://in.linkedin.com/in/dr-alkesh-agrawal-0050a9172" target="_blank" rel="noopener noreferrer" className="contact-box social linkedin">
-          <div className="social-icon">💼</div>
+          <div className="social-icon">
+            <img className='Contect-Icons' src="/src/assets/4.png" alt="LinkedIn" />
+          </div>
           <h2 className="social-name">LinkedIn</h2>
           <p className="social-desc">Connect professionally and explore academic networking opportunities.</p>
         </a>
 
         <a href="https://scholar.google.com/citations?hl=en&user=Yc6hSfcAAAAJ" target="_blank" rel="noopener noreferrer" className="contact-box social scholar">
-          <div className="social-icon">🎓</div>
+          <div className="social-icon">
+            <img className='Contect-Icons' src="/src/assets/5.png" alt="Google Scholar" />
+          </div>
           <h2 className="social-name">Google Scholar</h2>
           <p className="social-desc">Explore my complete list of indexed research publications and citations.</p>
         </a>
 
         <a href="https://publons.com/researcher/Q-5750-2019/" target="_blank" rel="noopener noreferrer" className="contact-box social facebook">
-          <div className="social-icon">👥</div>
+          <div className="social-icon">
+            <img className='Contect-Icons' src="/src/assets/1.png" alt="Web of Science" />
+          </div>
           <h2 className="social-name">Web of Science Researcher</h2>
           {/* <p className="social-desc">Follow my personal updates and community engagement.</p> */}
           <p className="social-desc">A verified record of my publications, citations, and peer review contributions.</p>
         </a>
 
         <a href="https://alkesh-agrawal.github.io/personal-web-page/myyoutubechannel.html" target="_blank" rel="noopener noreferrer" className="contact-box social youtube">
-          <div className="social-icon">▶️</div>
+          <div  className="social-icon">📺</div>
           <h2 className="social-name">YouTube</h2>
           <p className="social-desc">Watch my latest technical video lectures on digital communication.</p>
         </a>
